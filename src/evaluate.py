e16f0f0ca63df9d86e9609fe3e0906d66b2c6894
Andrew McDonald <andrewmcdonald27@gmail.com>
2023-06-29 01:04:13 +0100

    add one-class support for ssim and psnr

--- src/evaluate.py
+++ src/evaluate.py
@@ -6,6 +6,7 @@ import numpy as np
 import xarray as xr
 from sklearn.metrics import f1_score
 from dateutil.relativedelta import relativedelta
+from skimage.metrics import peak_signal_noise_ratio, structural_similarity
 
 
 def binary_accuracy(preds: xr.DataArray, target: xr.DataArray, mask: xr.DataArray, leadtimes_to_evaluate: list):
@@ -108,6 +109,156 @@ def sie_error(preds: xr.DataArray, target: xr.DataArray, mask: xr.DataArray, lea
 
     return mean_sie_error.item()
 
+
+def ssim(preds: xr.DataArray, target: xr.DataArray, leadtimes_to_evaluate: list):
+    """
+    Compute structural similarity score between preds and target to evaluate image structure.
+    Because this metric analyses holistic image structure, we do not mask it.
+    Expects preds to be a 3-channel probability map and target to be a 1-channel SIC map.
+    """
+    scores = []
+    for time in preds.time:
+        for leadtime in leadtimes_to_evaluate:
+            # can only evaluate one rgb image at a time
+            if len(preds.ice_class) == 1:
+                p = preds.sel(time=time, leadtime=leadtime).squeeze()
+                p_int = xr.where(p > 0.15, 1, 0)
+                p_int += xr.where(p > 0.85, 1, 0)
+                p_rgb = np.eye(3)[p_int]
+            else:
+                p_rgb = preds.sel(time=time, leadtime=leadtime).squeeze()
+            t = target.sel(time=time, leadtime=leadtime).squeeze()
+            t_int = xr.where(t > 0.15, 1, 0)
+            t_int += xr.where(t > 0.85, 1, 0)
+            t_rgb = np.eye(3)[t_int]
+            score = structural_similarity(t_rgb, p_rgb, data_range=1.0, channel_axis=-1)
+            scores.append(score)
+
+    return np.mean(scores)
+
+
+def psnr(preds: xr.DataArray, target: xr.DataArray, leadtimes_to_evaluate: list):
+    """
+    Compute peak-signal-to-noise ratio between preds and target to evaluate image structure.
+    Because this metric analyses holistic image structure, we do not mask it.
+    Expects preds to be a 3-channel probability map and target to be a 1-channel SIC map.
+    """
+    scores = []
+    for time in preds.time:
+        for leadtime in leadtimes_to_evaluate:
+            # can only evaluate one rgb image at a time
+            if len(preds.ice_class) == 1:
+                p = preds.sel(time=time, leadtime=leadtime).squeeze()
+                p_int = xr.where(p > 0.15, 1, 0)
+                p_int += xr.where(p > 0.85, 1, 0)
+                p_rgb = np.eye(3)[p_int]
+            else:
+                p_rgb = preds.sel(time=time, leadtime=leadtime).squeeze()
+            t = target.sel(time=time, leadtime=leadtime).squeeze()
+            t_int = xr.where(t > 0.15, 1, 0)
+            t_int += xr.where(t > 0.85, 1, 0)
+            t_rgb = np.eye(3)[t_int]
+            score = peak_signal_noise_ratio(t_rgb, p, data_range=1.0)
+            scores.append(score)
+
+    return np.mean(scores)
+
+
+def rapsd_field(field, fft_method=np.fft, return_freq=True, d=1.0, normalize=False, **fft_kwargs):
+    """
+    From https://github.com/pySTEPS/pysteps/blob/master/pysteps/utils/spectral.py#L100
+    Because this is the only function we use from pySTEPS, we copy it to avoid a 200mb install.
+    """
+    def compute_centred_coord_array(M, N):
+        """
+        From https://github.com/pySTEPS/pysteps/blob/master/pysteps/utils/arrays.py
+        """
+        if M % 2 == 1:
+            s1 = np.s_[-int(M / 2) : int(M / 2) + 1]
+        else:
+            s1 = np.s_[-int(M / 2) : int(M / 2)]
+
+        if N % 2 == 1:
+            s2 = np.s_[-int(N / 2) : int(N / 2) + 1]
+        else:
+            s2 = np.s_[-int(N / 2) : int(N / 2)]
+
+        YC, XC = np.ogrid[s1, s2]
+        return YC, XC
+
+    if len(field.shape) != 2:
+        raise ValueError(
+            f"{len(field.shape)} dimensions are found, but the number "
+            "of dimensions should be 2"
+        )
+
+    if np.sum(np.isnan(field)) > 0:
+        raise ValueError("input field should not contain nans")
+
+    m, n = field.shape
+
+    yc, xc = compute_centred_coord_array(m, n)
+    r_grid = np.sqrt(xc * xc + yc * yc).round()
+    l = max(field.shape[0], field.shape[1])
+
+    if l % 2 == 1:
+        r_range = np.arange(0, int(l / 2) + 1)
+    else:
+        r_range = np.arange(0, int(l / 2))
+
+    if fft_method is not None:
+        psd = fft_method.fftshift(fft_method.fft2(field, **fft_kwargs))
+        psd = np.abs(psd) ** 2 / psd.size
+    else:
+        psd = field
+
+    result = []
+    for r in r_range:
+        mask = r_grid == r
+        psd_vals = psd[mask]
+        result.append(np.mean(psd_vals))
+
+    result = np.array(result)
+
+    if normalize:
+        result /= np.sum(result)
+
+    if return_freq:
+        freq = np.fft.fftfreq(l, d=d)
+        freq = freq[r_range]
+        return result, freq
+    else:
+        return result
+
+
+def rapsd(preds: xr.DataArray, target: xr.DataArray, leadtimes_to_evaluate: list):
+    """
+    Compute radially-averaged power spectral density to evaluate image structure.
+    Because this metric analyses holistic image structure, we do not mask it.
+    Expects preds to be a 3-channel probability map and target to be a 1-channel SIC map.
+    """
+    preds_psd = {"no_ice": [], "marginal_ice": [], "full_ice": []}
+    target_psd = []
+    for time in preds.time:
+        for leadtime in leadtimes_to_evaluate:
+            for ice_class in ["no_ice", "marginal_ice", "full_ice"]:
+                # can only evaluate one 2D image at a time
+                p = preds.sel(time=time, leadtime=leadtime, ice_class=ice_class).squeeze()
+                p_psd, f = rapsd_field(p)
+                preds_psd[ice_class].append(p_psd)
+            t = target.sel(time=time, leadtime=leadtime).squeeze()
+            t_psd, f = rapsd_field(t)
+            target_psd.append(t_psd)
+
+    # make results into np arrays and take mean across forecasts
+    for ice_class in ["no_ice", "marginal_ice", "full_ice"]:
+        preds_psd[ice_class] = np.array(preds_psd[ice_class]).mean(axis=0)
+    target_psd = np.array(target_psd).mean(axis=0)
+    f =  np.array(f)
+
+    # f will be the same for all images since all images are the same size
+    return preds_psd, target_psd, f
+    
 
 def visualise_forecast(forecast_dict, forecast_mask, date, diff=False, true_forecast=None, ternerise=True):
 
